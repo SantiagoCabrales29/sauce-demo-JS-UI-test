770f8c9adc75c3d878327da73b063235935cbb01
scabrales <sacamo96@gmail.com>
2020-07-13 20:49:37 -0500

    Commit_02 - Login Feature: Unsuccessfull Login

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -6,10 +6,11 @@ describe('Log in Tests', () => {
     it('Successfull Login', () => {
         //browser.url('https://www.saucedemo.com/');
         browser.url("");
+        //expect(browser).toHaveTitle('Swag Labs');
         var username = $("#user-name");
         username.setValue("standard_user");
-        var asssert = username.getValue();
-        asssert.should.be.equals("standard_user");
+        //var asssert = username.getValue();
+        //asssert.should.be.equals("standard_user");
         var password = $("#password");
         password.setValue("secret_sauce");
         var loginButton = $("#login-button");
@@ -17,5 +18,21 @@ describe('Log in Tests', () => {
         //expect(browser).toHaveTitle('Swag Labs');
         var inventoryTitle = $("#inventory_filter_container > div").getText();
         expect(inventoryTitle).to.equals("Products");
+    })
+    it.only('Unsuccessful Login', () => {
+        browser.url("");
+        //expect(browser).toHaveTitle('Swag Labs');
+        var username = $("#user-name");
+        username.setValue("pro_user");
+        //var asssert = username.getValue();
+        //asssert.should.be.equals("standard_user");
+        var password = $("#password");
+        password.setValue("secret_sauce");
+        var loginButton = $("#login-button");
+        loginButton.click();     
+        //.login-box > form > h3
+        var message = $(".login-box > form > h3").getText();
+        expect(message).to.contain("Epic sadface");
+
     })
 })
