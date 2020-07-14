b8226134d5471fa88eaeefe47230dfca0a6ddeac
scabrales <sacamo96@gmail.com>
2020-07-13 20:34:24 -0500

    Commit_01 - Login Feature: Successful Login

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,7 +1,21 @@
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
+var chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+
+describe('Log in Tests', () => {
+    it('Successfull Login', () => {
         //browser.url('https://www.saucedemo.com/');
         browser.url("");
-        expect(browser).toHaveTitle('Swag Labs');
+        var username = $("#user-name");
+        username.setValue("standard_user");
+        var asssert = username.getValue();
+        asssert.should.be.equals("standard_user");
+        var password = $("#password");
+        password.setValue("secret_sauce");
+        var loginButton = $("#login-button");
+        loginButton.click();
+        //expect(browser).toHaveTitle('Swag Labs');
+        var inventoryTitle = $("#inventory_filter_container > div").getText();
+        expect(inventoryTitle).to.equals("Products");
     })
 })
